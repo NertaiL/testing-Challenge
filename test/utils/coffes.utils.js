a65b555b4f3b9ee8f682d61cee5f4bcd21e9714a
NertaiL <hans.latoja@gmail.com>
2024-02-03 14:03:51 -0300

    modification describe

--- test/utils/coffes.utils.js
+++
@@ -1,9 +0,0 @@
-import  pool  from "../../config/db/connectionDb.js";
-export const deletingCoffee = async (coffeeId) => {
-    const SQLquery = {
-      text: "DELETE FROM coffe WHERE id = $1",
-      values: [coffeeId],
-    };
-    
-    await pool.query(SQLquery);
-  };
